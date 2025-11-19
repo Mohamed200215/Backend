79415286b9b42fed9bab6f6f244976ba3898aa18
Mohamed200215 <maxamedmosseabdi@gmail.com>
2025-11-19 22:52:46 +0000

    Added logger middleware to track all backend requests

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
+
 const url = "mongodb+srv://maxamedmosseabdi:Maxamed12%3F%3F@lessons.1ojmdjj.mongodb.net/?appName=Lessons";
 const client = new MongoClient(url, {
   tls: true,
